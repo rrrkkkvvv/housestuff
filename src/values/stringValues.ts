e0e39444228c032955a0bfb9ec4dfd4add1c28db
Roman Karpii <romankarpiy8@gmail.com>
2024-08-06 22:35:06 +0200

    Changes to be committed: 	modified:   src/values/stringValues.ts
    Changes not staged for commit:
    	deleted:    housestuffBackend

--- src/values/stringValues.ts
+++ src/values/stringValues.ts
@@ -1,8 +1,16 @@
-export const stringValues = {
+export const errorMessage= "An error occurred. Please try again"
+export const authError = "Firstly you must login as admin"
+export const footerText = "All rights reserved &copy;"
+
+export const colors ={
     whiteColor: "#fff",
     darkGrayColor: "#333",
     lightGrayColor: "#eee",
     blackColor: "#000",
+
+}
+
+export const stringValues = {
     aboutUs: "We are a home furniture store, and we are selling the best stuff for your home",
     phoneNum: "+48000999222",
     facebook:"@HouseStuffFacebook",
@@ -12,19 +20,45 @@ export const stringValues = {
 
 export const titles = {
     aboutUs: "About us",
-    contacts: "Contact with us"
+    contacts: "Contact with us",
+    adminPanel: "Admin Panel",
+    categories:"Categories",
+    products:"Products",
+    editCategory: "Update/Delete categories",
+    editProdcut: "Update/Delete products",
 }
 
 export const popUpValues = {
     greenColor: "green",
     redColor: "red",
-    errorMessage: "An error occurred. Please try again",
-    categoriesValues:{
-        deleteConfirm: "Are you sure want to delete this category?",
-        successAdd : "Category was added successfully",
-        successDelete : "Category was deleted successfully",
-        successUpdate: "Category was updated successfully",
-    }
     
 }
- 
+
+
+export const categoriesValues={
+    deleteConfirm: "Are you sure want to delete this category?",
+    successAdd : "Category was added successfully",
+    successDelete : "Category was deleted successfully",
+    successUpdate: "Category was updated successfully",
+}
+export const producsValues ={
+    deleteConfirm: "Are you sure want to delete this product?",
+    successAdd : "Product was added successfully",
+    successDelete : "Product was deleted successfully",
+    successUpdate: "Product was updated successfully",
+
+}
+
+export const responseMessages = {
+    categoriesResponses:{
+        successAdd : "Category was created",
+        successDelete : "Category was deleted",
+        successUpdate: "Category was updated",
+    },
+    producsResponses :{
+        successAdd : "Product was created",
+        successDelete : "Product was deleted",
+        successUpdate: "Product was updated",
+    
+    }
+}
