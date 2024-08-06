c830a1dd87d6721e24b0be56aee94612865d46bd
Roman Karpii <romankarpiy8@gmail.com>
2024-08-06 22:33:31 +0200

    new file:   housestuffBackend 	new file:   src/values/stringValues.ts

---
+++ src/values/stringValues.ts
@@ -0,0 +1,30 @@
+export const stringValues = {
+    whiteColor: "#fff",
+    darkGrayColor: "#333",
+    lightGrayColor: "#eee",
+    blackColor: "#000",
+    aboutUs: "We are a home furniture store, and we are selling the best stuff for your home",
+    phoneNum: "+48000999222",
+    facebook:"@HouseStuffFacebook",
+    instagram:"@HouseStuffInst",
+    
+} 
+
+export const titles = {
+    aboutUs: "About us",
+    contacts: "Contact with us"
+}
+
+export const popUpValues = {
+    greenColor: "green",
+    redColor: "red",
+    errorMessage: "An error occurred. Please try again",
+    categoriesValues:{
+        deleteConfirm: "Are you sure want to delete this category?",
+        successAdd : "Category was added successfully",
+        successDelete : "Category was deleted successfully",
+        successUpdate: "Category was updated successfully",
+    }
+    
+}
+ 
