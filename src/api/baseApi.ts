49283d99e8a78ee46e689e1a889c34e916ef8b94
Roman Karpii <romankarpiy8@gmail.com>
2024-08-28 23:07:40 +0200

    Installed axios and was made a base query with it

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,7 +1,45 @@
-import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
+import { BaseQueryFn, createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
 import { apiURLs } from '../values/stringValues';
+import axios from 'axios';
+import type  { AxiosError, AxiosRequestConfig } from 'axios';
 
-const staggeredBaseQuery = retry(fetchBaseQuery({ baseUrl: apiURLs.baseURL }), {
+
+const axiosBaseQuery = 
+(
+  { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+): BaseQueryFn<
+  {
+    url: string
+    method?: AxiosRequestConfig['method']
+    data?: AxiosRequestConfig['data']
+    params?: AxiosRequestConfig['params']
+    headers?: AxiosRequestConfig['headers']
+  },
+  unknown,
+  unknown> => 
+    async ({url, method, data, params, headers})=>{
+    try{
+      const result = await axios({
+        url: baseUrl+url,
+        method,
+        data,
+        params,
+        headers,
+      })
+      return {data: result.data}
+    }catch(axiosError ){
+      const err = axiosError as AxiosError;
+      console.log( err.response?.data || err.message)
+      return {
+        error:{
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        }
+      }
+    }
+  }
+
+const staggeredBaseQuery = retry(axiosBaseQuery({ baseUrl: apiURLs.baseURL }), {
   maxRetries: 5,
 })
 const baseApi = createApi({
